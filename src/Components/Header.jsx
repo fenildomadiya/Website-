bfed17e86a8541c8f25de9713e69f6c3f7907667
fenil <fenilpatel669@gmail.com>
2023-11-09 15:30:03 +0530

    ghfty

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
+import { NavLink, Navigate } from 'react-router-dom';
 import styled from 'styled-components';
+import Navbar from './Navbar';
 
 const Header = () => {
   return (
     <>
+    
       <MainHeader>
-        
+        <NavLink to="/">
+            <img src="images/logo.png" alt="Logo-img" className='logo'/>
+        </NavLink>
+        <Navbar/>
+
       </MainHeader>
     </>
   )
 };
 
 const MainHeader = styled.header `
-padding:0 4.8 rem;
+padding:0 4rem;
 height:10rem;
 background-color:${({theme}) => theme.colors.bg};
 justify-content:space-between;
@@ -22,7 +29,7 @@ position:relative;
 
 
 .logo{
-    height:5rem;
+    height:8rem;
 }
 
 `;
