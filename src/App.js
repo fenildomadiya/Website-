a8a171b01b6330ad037a398062e8720f387c01ee
fenil <fenilpatel669@gmail.com>
2023-11-09 14:00:09 +0530

    jh

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route,Routes } from "react-router-dom";
 import Home from "./Home";
-
+import About from "./About";
+import Products from "./Products";
+import Contact from "./Contact";
+import SingleProduct from "./SingleProduct";
+import Cart from "./Cart"
+ 
 const App = () => {
   return (
     <>
@@ -11,6 +16,16 @@ const App = () => {
 
         <Route path="/" element={<Home/>}></Route>
         <Route path="/about" element={<About/>}></Route>
+
+  <Route path="/products" element={<Products/>}></Route>
+
+  <Route path="/contact" element={<Contact/>}></Route>
+
+<Route path="/singleproduct/:id" element={<SingleProduct/>}></Route>
+
+<Route path="/cart" element={<Cart/>}></Route>
+
+
 
       </Routes>
 
